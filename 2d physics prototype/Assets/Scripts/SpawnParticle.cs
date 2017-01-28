395b9e156337ce830bbbfa5e9b52a9ad445a4973
Tom Cebula <MindlessShad@gmail.com>
2017-01-28 14:38:59 +0000

    Finished basic collision between stuff, added nice spawning function on mouse position

---
+++ 2d physics prototype/Assets/Scripts/SpawnParticle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticle : MonoBehaviour
+{
+
+    Ray ray;
+    RaycastHit hit;
+    public GameObject pixel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+            }
+
+           GameObject obj = Instantiate(pixel, hit.point, Quaternion.identity) as GameObject;
+        }
+    }
+
+
+}
